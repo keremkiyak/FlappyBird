032c66b28a330afd6eebdc200fb8ec12201baafd
keremkiyak <110934008+keremkiyak@users.noreply.github.com>
2024-04-08 12:59:40 +0300

    created homepage and bird

---
+++ flappy_bird_application/lib/homepage.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flappy_bird_application/bird.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double birdY = 0;
+  void jump() {
+    Timer.periodic(Duration(milliseconds: 50), (timer) {
+      setState(() {
+        birdY -= 0.05;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: jump,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Stack(
+                    children: [
+                      MyBird (
+                        birdY: birdY,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.brown,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
