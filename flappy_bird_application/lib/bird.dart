7ef7a7ee2b68bbf692e55fccc8e4befa2ad09f33
keremkiyak <110934008+keremkiyak@users.noreply.github.com>
2024-04-08 14:01:37 +0300

    changed bird image

--- flappy_bird_application/lib/bird.dart
+++ flappy_bird_application/lib/bird.dart
@@ -10,9 +10,8 @@ final birdY;
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment(0, birdY),
-      height: 50,
-      width: 50,
-      child: Image.asset('images/bird.jpeg'),
+      
+      child: Image.asset('images/bird.jpeg', width: 50),
     );
   }
 }
--- flappy_bird_application/lib/homepage.dart
+++ flappy_bird_application/lib/homepage.dart
@@ -10,19 +10,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double birdY = 0;
-  void jump() {
-    Timer.periodic(Duration(milliseconds: 50), (timer) {
+  static double birdY = 0;
+  double initialPosition = birdY;
+  double height = 0;
+  double time = 0;
+  double gravity = -4.9; //how strong the gravity.
+  double velocity = 3.5; //how strong the jump.
+
+  bool gameHasStarted = false;
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 10), (timer) {
+      height = gravity * time * time + 30 * time;
       setState(() {
-        birdY -= 0.05;
+        birdY = initialPosition - height;
       });
+
+      if(birdIsDead()){
+        timer.cancel();
+        gameHasStarted = false;
+      //  _showDialog();
+
+      }
+
+      if (birdY < -1 || birdY > 1) {
+        timer.cancel();
+      }
+      time += 0.01;
     });
+  }
+
+  void jump() {
+    setState(() {
+      time = 0;
+      initialPosition = birdY;
+    });
+  }
+
+  bool birdIsDead() {
+    if(birdY < -1 || birdY > 1){
+      return true;
+    }
+//check the bird is hitting a barrier. 
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: jump,
+      onTap: gameHasStarted ? jump : startGame,
       child: Scaffold(
         body: Column(
           children: [
@@ -33,9 +70,17 @@ class _HomePageState extends State<HomePage> {
                 child: Center(
                   child: Stack(
                     children: [
-                      MyBird (
+                      MyBird(
                         birdY: birdY,
-                      )
+                      ),
+                      Container(
+                        alignment: Alignment(0, -0.5),
+                        child: Text( gameHasStarted? '' : 'T A P  T O  P L A Y',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            )),
+                      ),
                     ],
                   ),
                 ),
